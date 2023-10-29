f584d5be3e36bac818af2d16e3131fb491e497e4
VinzentSchmid <vinzent.schmid@edu.fh-joanneum.at>
2023-10-30 00:03:51 +0100

    SMS-7: added login and dashboard component and added styling for login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomInputFieldComponent } from './custom-input-field/custom-input-field.component';
+import { LoginComponent } from './login/login.component';
+import {NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomInputFieldComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage,
+    FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/custom-input-field/custom-input-field.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, Type} from '@angular/core';
+
+@Component({
+  selector: 'app-custom-input-field',
+  templateUrl: './custom-input-field.component.html',
+  styleUrls: ['./custom-input-field.component.scss']
+})
+export class CustomInputFieldComponent {
+  @Input() label: string = 'Default Label';
+  @Input() type: string = 'text';
+
+  isInputFocused = false;
+
+  onInputFocus() {
+    this.isInputFocused = true;
+  }
+
+  onInputBlur() {
+    this.isInputFocused = false;
+  }
+
+  onInputValueChange(value: string) {
+    // Add any additional logic when the input value changes if needed
+  }
+}
